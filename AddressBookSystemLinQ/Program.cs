15da5414cca18fbe397835e3d8de5f61e2c661c0
peermohsin22 <peermohsin22@gmail.com>
2022-02-06 13:00:12 +0530

    Create new Data Table

---
+++ AddressBookSystemLinQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookSystemLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to  Address Book Using LINQ! ");
+
+            // UC 1 Create a new table
+            AddressBookTable addressBookTable = new AddressBookTable();
+        }
+    }
+}
